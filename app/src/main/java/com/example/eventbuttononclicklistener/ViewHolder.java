8ae5df53f3b36c9e78eee4b8387fcf8d5bdbb400
dwiaprilyaanggoroputry <dwi.aprilya.ft20@mail.umy.ac.id>
2022-03-29 15:20:03 +0700

    Activity 5 Aplikasi Android dengan Menggunakan Popup Menu dan Listview.

---
+++ app/src/main/java/com/example/eventbuttononclicklistener/ViewHolder.java
@@ -0,0 +1,8 @@
+package com.example.eventbuttononclicklistener;
+
+import android.widget.TextView;
+
+public class ViewHolder {
+    // mendeklarasikan variable dengan jenis TextView
+    TextView name;
+}
